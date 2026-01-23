0a12d0a5c0a8cb33f90cb41d05a121cce3d1fbb3
Winston Zhao <winstonzhao@ucla.edu>
2026-01-22 21:06:41 -0800

    feat: Add initial Cask for v0.6.6

---
+++ Casks/back2vibing.rb
@@ -0,0 +1,27 @@
+cask "back2vibing" do
+  version "0.6.6"
+  sha256 "e469dd6e03362ca2b197e2d7362eb86444844ac3c9f3cdb49bd66f830207d737"
+
+  url "https://github.com/builtby-win/back2vibing/releases/download/v#{version}/back2vibing_#{version}_aarch64.dmg"
+  name "Back2Vibing"
+  desc "Desktop companion that keeps you focused while long-running AI coding tasks finish"
+  homepage "https://builtby.win/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  auto_updates true
+  depends_on macos: ">= :high_sierra"
+  arch arm: "aarch64", intel: "x64"
+
+  app "Back2Vibing.app"
+
+  zap trash: [
+    "~/Library/Application Support/back2vibing",
+    "~/Library/Caches/back2vibing",
+    "~/Library/Preferences/com.builtby-win.back2vibing.plist",
+    "~/Library/WebKit/com.builtby-win.back2vibing",
+  ]
+end
