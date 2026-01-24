b4f1eac1464cc194f030b89ce56932f9b00397f4
Winston Zhao <winstonzhao@ucla.edu>
2026-01-23 16:33:01 -0800

    feat: Update back2vibing to v0.6.7

--- Casks/back2vibing.rb
+++ Casks/back2vibing.rb
@@ -1,6 +1,6 @@
 cask "back2vibing" do
-  version "0.6.6"
-  sha256 "e469dd6e03362ca2b197e2d7362eb86444844ac3c9f3cdb49bd66f830207d737"
+  version "0.6.7"
+  sha256 "d0b97942cda2d4580fe899199818e965f052f2221572509fe892e283beb1dd5e"
 
   url "https://github.com/builtby-win/back2vibing/releases/download/v#{version}/back2vibing_#{version}_aarch64.dmg",
       verified: "github.com/builtby-win/back2vibing/"
